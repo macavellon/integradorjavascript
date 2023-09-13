087ba28efc7eccb84bd5518f451d2c6c0ec244a7
maca.vn94@gmail.com <maca.vn94@gmail.com>
2023-09-13 15:18:22 -0300

    cambios

--- assets/index.js
+++ assets/index.js
@@ -37,7 +37,7 @@ const createProductTemplate = (product) => {
                     data-id='${id}'
                     data-name='${name}'
                     data-precio='${precio}'
-                    data-img='${cardImg}'>Add</button>
+                    data-img='${cardImg}'>Comprar/button>
                 </div>
             </div>
         </div>
